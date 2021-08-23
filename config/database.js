69e29ae0605cb2227f33e6e91d9b828e2dee9a65
Francisco  Carlos <franciscocarlos@MacBook-Pro-de-Francisco.local>
2021-08-23 08:45:50 -0300

    fisrt commit

---
+++ config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+  dialect: 'mysql',
+  host: 'localhost',
+  username: 'root',
+  password: 'arena200',
+  database: 'digitalmenu',
+  define: {
+    timestamps: true,
+    underscored: true,
+  }
+};
---
+++ src/routes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const routes = express.Router();
+
+routes.get('/', (req, res) => {
+  return res.json({msg: 'API DigitalMenu Online'});
+});
+
+
+module.exports = routes;
