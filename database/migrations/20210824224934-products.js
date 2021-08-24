954fa6cf4d5999828db6f79a6a2a6d5b8b949481
Francisco  Carlos <franciscocarlos@MacBook-Pro-de-Francisco.local>
2021-08-24 19:52:03 -0300

    feat: create categories, products and image_products migrations

---
+++ database/migrations/20210824224934-products.js
@@ -0,0 +1,59 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+ 
+    return await queryInterface.createTable('products', { 
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      company_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: 'master_companies', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      category_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: 'categories', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      title: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      subtitle: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      active: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      }
+    });
+     
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
---
+++ database/migrations/20210824225042-image_products.js
@@ -0,0 +1,48 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+ 
+    return await queryInterface.createTable('image_products', { 
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      product_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: 'products', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      img_url: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      active: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      }
+    });
+     
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
