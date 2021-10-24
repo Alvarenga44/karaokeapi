e92a3f68e355966872988a6e5bc8c33d9e0858f3
Francisco Alvarenga <francisco@athoslabs.com.br>
2021-10-24 18:46:56 -0300

    chore: change package.json scripts

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ module.exports = {
   dialect: 'mysql',
   host: 'localhost',
   username: 'root',
-  password: 'arena200',
+  password: '',
   database: 'digitalmenu',
   define: {
     timestamps: true,
